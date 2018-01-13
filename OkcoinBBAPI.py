96560e751edea719e4838d461e829106503720d7
zhezhong <897359054@qq.com>
2018-01-14 01:49:34 +0800

    bibi

---
+++ OkcoinBBAPI.py
@@ -0,0 +1,155 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# 用于访问OKCOIN 币币交易Api REST API
+from HttpMD5Util import buildMySign, httpGet, httpPost
+import logging
+import time
+import json
+import os
+import pandas as pd
+import pandas.stats.moments
+import numpy as np
+import sys
+from datetime import datetime
+
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+# 用于访问OKCOIN 现货REST API
+from HttpMD5Util import buildMySign, httpGet, httpPost
+
+
+class OKCoinBibi:
+
+    def __init__(self, url, apikey, secretkey):
+        self.__url = url
+        self.__apikey = apikey
+        self.__secretkey = secretkey
+
+    #use 获取OKCOIN现货行情信息  公开的，不要Api。
+    def ticker(self, symbol=''):
+        TICKER_RESOURCE = "/api/v1/ticker.do"
+        params = ''
+        if symbol:
+            params = 'symbol=%(symbol)s' % {'symbol': symbol}
+        return httpGet(self.__url, TICKER_RESOURCE, params)
+
+    # use 获取OKCOIN现货市场深度
+    def depth(self, symbol='', size=5):
+        DEPTH_RESOURCE = "/api/v1/depth.do"
+        params = {
+            'symbol': symbol,
+            'size': size
+        }
+        return httpGet(self.__url, DEPTH_RESOURCE, params)
+
+    # use获取OKCOIN现货历史交易信息
+    def trades(self, symbol=''):
+        TRADES_RESOURCE = "/api/v1/trades.do"
+        params = ''
+        if symbol:
+            params = 'symbol=%(symbol)s' % {'symbol': symbol}
+        return httpGet(self.__url, TRADES_RESOURCE, params)
+
+    # use 获取货币合约K线信息
+    def kline(self, symbol, contractType, type, size=0, since=0):
+        # print(symbol,contractType,type,size)
+        FUTURE_USERINFO = "/api/v1/kline.do"
+        params = {
+            'symbol':symbol,
+            'type':type,# frequenncy
+            'size':size,
+            'since':since,
+        }
+        # print('-------',params)
+        return httpGet(self.__url, FUTURE_USERINFO, params)
+
+    # use获取用户现货账户信息
+    def userinfo(self):
+        USERINFO_RESOURCE = "/api/v1/userinfo.do"
+        params = {}
+        params['api_key'] = self.__apikey
+        params['sign'] = buildMySign(params, self.__secretkey)
+        return httpPost(self.__url, USERINFO_RESOURCE, params)
+
+    # use币币交易交易
+    def trade(self, symbol, tradeType, price='', amount=''):
+        TRADE_RESOURCE = "/api/v1/trade.do"
+        params = {
+            'api_key': self.__apikey,
+            'symbol': symbol,
+            'type': tradeType # buy sell buy_market,sell_market
+        }
+        if price:
+            params['price'] = price
+        if amount:
+            params['amount'] = amount
+
+        params['sign'] = buildMySign(params, self.__secretkey)
+        return httpPost(self.__url, TRADE_RESOURCE, params)
+
+    # 现货批量下单
+    def batchTrade(self, symbol, tradeType, orders_data):
+        BATCH_TRADE_RESOURCE = "/api/v1/batch_trade.do"
+        params = {
+            'api_key': self.__apikey,
+            'symbol': symbol,
+            'type': tradeType,
+            'orders_data': orders_data
+        }
+        params['sign'] = buildMySign(params, self.__secretkey)
+        return httpPost(self.__url, BATCH_TRADE_RESOURCE, params)
+
+    # 现货取消订单
+    def cancelOrder(self, symbol, orderId):
+        CANCEL_ORDER_RESOURCE = "/api/v1/cancel_order.do"
+        params = {
+            'api_key': self.__apikey,
+            'symbol': symbol,
+            'order_id': orderId
+        }
+        params['sign'] = buildMySign(params, self.__secretkey)
+        return httpPost(self.__url, CANCEL_ORDER_RESOURCE, params)
+
+    # 现货订单信息查询
+    def orderinfo(self, symbol, orderId):
+        ORDER_INFO_RESOURCE = "/api/v1/order_info.do"
+        params = {
+            'api_key': self.__apikey,
+            'symbol': symbol,
+            'order_id': orderId
+        }
+        params['sign'] = buildMySign(params, self.__secretkey)
+        return httpPost(self.__url, ORDER_INFO_RESOURCE, params)
+
+    # 现货批量订单信息查询
+    # 需要用到orders info 了。哈哈哈，批量订单查询。
+    def ordersinfo(self, symbol, orderId, tradeType):
+        ORDERS_INFO_RESOURCE = "/api/v1/orders_info.do"
+        params = {
+            'api_key': self.__apikey,
+            'symbol': symbol,
+            'order_id': orderId,
+            'type': tradeType
+        }
+        params['sign'] = buildMySign(params, self.__secretkey)
+        return httpPost(self.__url, ORDERS_INFO_RESOURCE, params)
+
+    # 现货获得历史订单信息
+    def orderHistory(self, symbol, status, currentPage, pageLength):
+        ORDER_HISTORY_RESOURCE = "/api/v1/order_history.do"
+        params = {
+            'api_key': self.__apikey,
+            'symbol': symbol,
+            'status': status,
+            'current_page': currentPage,
+            'page_length': pageLength
+        }
+        params['sign'] = buildMySign(params, self.__secretkey)
+        return httpPost(self.__url, ORDER_HISTORY_RESOURCE, params)
+
+
+
+    # use获取美元人民币汇率
+    def exchange_rate(self):
+        EXCHANGE_RATE = "/api/v1/exchange_rate.do"
+        return httpGet(self.__url, EXCHANGE_RATE, '')
